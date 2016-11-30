bbebd6745e6dad26ea7d9c79e41705417689c829
PanayiotisPh <pr.filippou@gmail.com>
2016-11-30 15:23:48 +0200

    02 BullCowGame Creating Functions

--- BullCowGame/02_Solution/BullCowGame/main.cpp
+++ BullCowGame/02_Solution/BullCowGame/main.cpp
@@ -1,6 +1,34 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
+
+//Functions
+void PrintIntro();
+string GetGuessAndPrintBack();
 
 int main()
 {
-	std::cout << "We will start making the BullCowGame\n";
+	PrintIntro();
+	GetGuessAndPrintBack();
+	return 0;
+}
+
+void PrintIntro()
+{
+	// Introduce the game
+	constexpr int WORD_LENGHT = 5; // Apofygh "magic numbers", eukolh allagh metavliths se olo to programma.
+	cout << "Welcome to BullCowGame game!\n"
+		 << "Can you guess the " << WORD_LENGHT
+		 << "th letter isogram I'm thinking of? \n";
+	return;
+}
+string GetGuessAndPrintBack()
+{
+	// Get a guess from the player.
+	cout << "Please enter your Guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	cout << "Your quess was: " << Guess << endl;
+	return Guess;
 }
