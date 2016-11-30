6b60485b835a0e3e0a5badc6b6a263a6839b2787
PanayiotisPh <pr.filippou@gmail.com>
2016-11-30 22:38:29 +0200

    03 BullCowGame Intro to classes

--- BullCowGame/02_Solution/BullCowGame/main.cpp
+++ BullCowGame/02_Solution/BullCowGame/main.cpp
@@ -3,32 +3,53 @@
 
 using namespace std;
 
-//Functions
 void PrintIntro();
-string GetGuessAndPrintBack();
-
+void PlayGame();
+string GetGuess();
+bool AskToPlayAgain();
 int main()
 {
-	PrintIntro();
-	GetGuessAndPrintBack();
+	bool bPlayAgain = false;
+	do
+	{
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+	} while (bPlayAgain);
 	return 0;
 }
-
 void PrintIntro()
 {
 	// Introduce the game
-	constexpr int WORD_LENGHT = 5; // Apofygh "magic numbers", eukolh allagh metavliths se olo to programma.
+	constexpr int WORD_LENGHT = 2; // Apofygh "magic numbers", eukolh allagh metavliths se olo to programma.
 	cout << "Welcome to BullCowGame game!\n"
-		 << "Can you guess the " << WORD_LENGHT
-		 << "th letter isogram I'm thinking of? \n";
+		<< "Can you guess the " << WORD_LENGHT
+		<< "th letter isogram I'm thinking of? \n";
 	return;
 }
-string GetGuessAndPrintBack()
+void PlayGame()
+{
+	constexpr int NUM_GUESSES = 5;
+	for (int count = 1; count <= NUM_GUESSES; count++)
+	{
+		string Guess = GetGuess();
+		cout <<"Your Guess Is: "<< Guess << endl;
+	}
+}
+string GetGuess()
 {
 	// Get a guess from the player.
 	cout << "Please enter your Guess: ";
 	string Guess = "";
 	getline(cin, Guess);
-	cout << "Your quess was: " << Guess << endl;
 	return Guess;
+}
+bool AskToPlayAgain()
+{
+	cout << "Do You Want To Play Again? (y,n)\n";
+	string Response = "";
+	getline(cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
+
+
 }
